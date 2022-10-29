15e505f5b4563c080182d159a96a4845e28ec54f
ollanket <onni.ollanketo98@gmail.com>
2022-10-29 05:48:47 +0300

    Add basic implementation idea

--- main.py
+++ main.py
@@ -8,8 +8,13 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
   try:
-    print(msg.topic+" "+str(msg.payload))
-    # Get bytes & create a file from them & convert to pdf with ghostpdl & print job
+    # Get bytes & create a file from them & convert to pdf with ghostpdl
+    data = str(msg.payload)
+    with open("tempfile.PRN", "wb") as binary_file:
+      binary_file.write(bytes.fromhex(data))
+    os.system('./ghostpdl-10.0.0/bin/gpcl6 -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile=output.pdf tempfile.PRN')
+    # TODO: print pdf to usb connected printer
+    # TODO: push pdf somewhere where it can be downloaded.
   except Exception as e:
     logging.error("Error parsing message")
     logging.exception(e)
@@ -21,11 +26,3 @@ client.on_message = on_message
 client.connect("localhost", 1883, 60)
 
 client.loop_forever()
-
-
-# bytes = bytes.fromhex('0000000000000000000200620000000000000200002000000000000000000000301B0000000000000008020080080004800000200200620000000000000200002000000000000000000000301B000C')
-
-# with open("testfile.PRN", "wb") as binary_file:
-#   binary_file.write(bytes)
-
-# os.system('./ghostpdl-10.0.0/bin/gpcl6 -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile=output.pdf testfile.PRN')
